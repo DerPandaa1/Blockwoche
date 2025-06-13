b9aa7cf821a1400553f7c661911d5fd18ed2dc25
Fabian Fuchs <github@ffuchs.dev>
2025-06-13 08:49:00 +0200

    feat: nixos support

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "DBBenchmark";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python313Packages.python
+            pkgs.python313Packages.pip
+            pkgs.python313Packages.poetry-core
+            pkgs.bashInteractive
+          ];
+        };
+      });
+}
